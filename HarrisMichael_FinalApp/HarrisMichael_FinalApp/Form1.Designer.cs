0a4bd08f4340f58423c450cb7b3a68c4a19feecb
MichaelH98 <bigmikeharris@gmail.com>
2020-01-12 14:31:04 -0500

    Added to Repo

---
+++ HarrisMichael_FinalApp/HarrisMichael_FinalApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace HarrisMichael_FinalApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AppBackGroundColor = new System.Windows.Forms.PictureBox();
+            this.MoiveGB = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AppBackGroundColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AppBackGroundColor
+            // 
+            this.AppBackGroundColor.Location = new System.Drawing.Point(48, 167);
+            this.AppBackGroundColor.Name = "AppBackGroundColor";
+            this.AppBackGroundColor.Size = new System.Drawing.Size(575, 982);
+            this.AppBackGroundColor.TabIndex = 0;
+            this.AppBackGroundColor.TabStop = false;
+            // 
+            // MoiveGB
+            // 
+            this.MoiveGB.Location = new System.Drawing.Point(48, 245);
+            this.MoiveGB.Name = "MoiveGB";
+            this.MoiveGB.Size = new System.Drawing.Size(575, 904);
+            this.MoiveGB.TabIndex = 1;
+            this.MoiveGB.TabStop = false;
+            this.MoiveGB.Text = "Moives";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::HarrisMichael_FinalApp.Properties.Resources.iPhone7Image;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(661, 1280);
+            this.Controls.Add(this.MoiveGB);
+            this.Controls.Add(this.AppBackGroundColor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.AppBackGroundColor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AppBackGroundColor;
+        private System.Windows.Forms.GroupBox MoiveGB;
+    }
+}
+
---
+++ HarrisMichael_FinalApp/HarrisMichael_FinalApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace HarrisMichael_FinalApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Written by Keith Webster.  Used with permission.  Not to be distributed.  
+            //Place this inside the class space in the form you would like to size.
+            //Call this method AFTER InitializeComponent() inside the form's constructor.
+            void HandleClientWindowSize()
+            {
+                //Modify ONLY these float values
+                float HeightValueToChange = 1.4f;
+                float WidthValueToChange = 6.0f;
+
+                //DO NOT MODIFY THIS CODE
+                int height = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Height / HeightValueToChange);
+                int width = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Width / WidthValueToChange);
+                if (height < Size.Height)
+                    height = Size.Height;
+                if (width < Size.Width)
+                    width = Size.Width;
+                this.Size = new Size(width, height);
+                //this.Size = new Size(376, 720);
+            }
+        }
+
+        //Api Connection
+        WebClient apiConnection = new WebClient();
+        string startApi = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Avengers%20Endgame/apikey=344ffb430bmsh8c907a15aaf7c31p166f05jsnc09e2c731b9a";
+        string apiEndpoint;
+
+
+    }
+}
