4572dea6024333a977177f61ade99e4077780be7
MichaelH98 <bigmikeharris@gmail.com>
2020-01-22 12:07:24 -0500

    ADD--Added project to repo

---
+++ HarrisMichael_FillLogic/HarrisMichael_FillLogic/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class frmTicTacToe : Form
+    {
+        // NAME:
+        // CLASS AND TERM:
+        // PROJECT: Tic Tac Toe
+
+        /* THINGS TO CONSIDER:
+            - You must change the project name to conform to the required
+              naming convention.
+            - You must comment the code throughout.  Failure to do so could result
+              in a lower grade.
+            - All button names and other provided variables and controls must
+              remain the same.  Changing these could result in a 0 on the project.
+            - Selecting Blue or Red on the View menu should change the imageList
+              attached to all buttons so that any current play will change the color
+              of all button images.
+            - Saved games should save to XML.  A game should load only from XML and
+              should not crash the application if a user tries to load an incorrect 
+              file.
+        */
+
+        public frmTicTacToe()
+        {
+            InitializeComponent();
+        }
+    }
+}
